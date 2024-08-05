6aa45e550410ab489a068910e09b5d8d1720db15
Sylvainxiii <sylvainlacroix@protonmail.com>
2024-08-05 18:27:45 +0200

    Seconde version du router améliorée

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Billetterie from "./vues/Billetterie.js";
 import Contact from "./vues/Contact.js";
 import Crepuscule from "./vues/Crepuscule.js";
 import Drac from "./vues/Drac.js";
-import Engagerments from "./vues/Engagements.js";
+import Engagements from "./vues/Engagements.js";
 import Info from "./vues/Info.js";
 import Partenaires from "./vues/Partenaires.js";
 import Programmation from "./vues/Programmation.js";
@@ -13,37 +13,29 @@ const App = () => {
 
     const app = document.createElement('div');
     app.className = 'app';
-    const currentPath = window.location.pathname;
-    console.log(currentPath);
 
-    switch (currentPath) {
-        case '/':
-            Accueil(app);
-            break;
-        case '/crepuscule/':
-            Crepuscule(app);
-            break;
-        case '/programmation/':
-            Programmation(app);
-            break;
-        case '/billetterie/':
-            Billetterie(app);
-            break;
-        case '/infos-pratiques/':
-            Info(app);
-            break;
-        case '/engagements/':
-            Engagerments(app);
-            break;
-        case '/partenaires/':
-            Partenaires(app);
-            break;
-        case '/dracevents/':
-            Drac(app);
-            break;
-        case '/contact/':
-            Contact(app);
-            break;
+    const currentPath = window.location.pathname;
+
+    const routes = {
+        '/': Accueil,
+        '/crepuscule/': Crepuscule,
+        '/programmation/': Programmation,
+        '/billetterie/': Billetterie,
+        '/infos-pratiques/': Info,
+        '/engagements/': Engagements,
+        '/partenaires/': Partenaires,
+        '/dracevents/': Drac,
+        '/contact/': Contact
+    };
+
+    const Component = routes[currentPath];
+
+    if (Component) {
+        Component(app);
+    } else {
+        const notFound = document.createElement('div');
+        notFound.textContent = 'Page non trouvée';
+        app.appendChild(notFound);
     }
 
     return app;
