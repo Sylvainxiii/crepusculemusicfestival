711616808523b35909bd3fa64b160b815de0b227
Sylvainxiii <sylvainlacroix@protonmail.com>
2024-09-06 14:55:14 +0200

    Modification de la banniere avec portail

--- src/components/Banniere/Banniere.js
+++ src/components/Banniere/Banniere.js
@@ -1,33 +1,10 @@
 import './Banniere.css';
-import teaser from "/src/assets/video/teaser.mp4";
-import img from "/src/assets/images/fond-accueil.webp";
-import logo from "/src/assets/images/logo.svg";
+import Portail from '/src/components/Portail/Portail';
+import logo from "/src/assets/images/crepuscule-blanc.webp";
 
 const Banniere = () => {
 
   const fragment = document.createDocumentFragment();
-  const videoContainer = document.createElement('div');
-  videoContainer.id = "container-video-banniere";
-  videoContainer.className = "relative";
-
-  const video = document.createElement('video');
-  video.id = "video-banniere";
-  video.className = "absolute";
-  video.muted = true;
-  video.autoplay = true;
-  video.playsInline = true;
-  video.loop = true;
-
-  const sourcevideo = document.createElement('source');
-  sourcevideo.src = teaser;
-  sourcevideo.type = "video/mp4";
-  video.appendChild(sourcevideo);
-
-  const imgBanniere = document.createElement('img');
-  imgBanniere.id = "img-banniere";
-  imgBanniere.src = img;
-  imgBanniere.className = "absolute";
-  imgBanniere.alt = "Crepuscule Music Festival ALBI"
 
   const logoBanniere = document.createElement('img');
   logoBanniere.id = "logo-banniere";
@@ -35,10 +12,8 @@ const Banniere = () => {
   logoBanniere.src = logo;
   logoBanniere.alt = "Crepuscule Music Festival logo"
 
-  videoContainer.appendChild(video);
-  fragment.appendChild(videoContainer);
   fragment.appendChild(logoBanniere);
-  fragment.appendChild(imgBanniere);
+  fragment.appendChild(Portail());
 
   const banniere = document.createElement('div');
   banniere.id = 'banniere'
