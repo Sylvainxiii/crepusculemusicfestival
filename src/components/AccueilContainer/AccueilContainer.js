353ef04d5fc9fb007dd7b13aab67adc08746251f
Sylvainxiii <sylvainlacroix@protonmail.com>
2024-09-10 15:31:47 +0200

    Grille programmation ebauche avec vidéo et artefact en three.js

--- src/components/AccueilContainer/AccueilContainer.js
+++ src/components/AccueilContainer/AccueilContainer.js
@@ -1,17 +1,8 @@
 import "./AccueilContainer.css";
 
-const AccueilContainer = (id, titre) => {
+const AccueilContainer = () => {
     const accueilContainer = document.createElement('div');
-    accueilContainer.classList.add("accueil-container", "relative");
-
-    const titreAccueilContainer = document.createElement('h1');
-    titreAccueilContainer.id = id;
-    titreAccueilContainer.textContent = titre;
-    accueilContainer.appendChild(titreAccueilContainer);
-
-    const borderAccueilContainer = document.createElement('div');
-    borderAccueilContainer.classList.add("border-accueil-container", "absolute");
-    accueilContainer.appendChild(borderAccueilContainer);
+    accueilContainer.classList.add("accueil-container", "relative", "flex-column", "center");
 
     return accueilContainer;
 }
